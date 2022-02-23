a62c099684c98891e49e27cb4b81093754f99073
Nikolay Tuzov <just.skiv@gmail.com>
2022-02-23 14:26:36 +0300

    fix y.metrika counter

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,12 +22,8 @@ module.exports = {
     ['link', {rel: "shortcut icon", href: "/favicon.ico"}],
 
     //yandex-metrika
-    ['script', {}, `
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(82930849, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });
-    `],
-    ['noscript', {}, `
-        <div><img src="https://mc.yandex.ru/watch/82930849" style="position:absolute; left:-9999px;" alt="" /></div>
-    `],
+    ['script', {}, `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(87611844, "init", {clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true});`],
+    ['noscript', {}, `<div><img src="https://mc.yandex.ru/watch/87611844" style="position:absolute; left:-9999px;" alt="" /></div>`],
 
     //remark42
     ['script', {}, `var remark_config = {
